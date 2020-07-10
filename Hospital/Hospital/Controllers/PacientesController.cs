ad82a8c9af8c2d4d6cac3cff70c5956dee7b1e35
ferguzaja <ferguzaja@gmail.com>
2020-07-09 21:42:04 -0500

    api
    api

---
+++ Hospital/Hospital/Controllers/PacientesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Hospital.Models;
+
+namespace Hospital.Controllers
+{
+    public class PacientesController : ApiController
+    {
+        private HospitalEntities db = new HospitalEntities();
+
+        // GET: api/Pacientes
+        public IQueryable<Paciente> GetPaciente()
+        {
+            return db.Paciente;
+        }
+
+        // GET: api/Pacientes/5
+        [ResponseType(typeof(Paciente))]
+        public IHttpActionResult GetPaciente(string id)
+        {
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(paciente);
+        }
+
+        // PUT: api/Pacientes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPaciente(string id, Paciente paciente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != paciente.Curp)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(paciente).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PacienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Pacientes
+        [ResponseType(typeof(Paciente))]
+        public IHttpActionResult PostPaciente(Paciente paciente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Paciente.Add(paciente);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PacienteExists(paciente.Curp))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = paciente.Curp }, paciente);
+        }
+
+        // DELETE: api/Pacientes/5
+        [ResponseType(typeof(Paciente))]
+        public IHttpActionResult DeletePaciente(string id)
+        {
+            Paciente paciente = db.Paciente.Find(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            db.Paciente.Remove(paciente);
+            db.SaveChanges();
+
+            return Ok(paciente);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PacienteExists(string id)
+        {
+            return db.Paciente.Count(e => e.Curp == id) > 0;
+        }
+    }
+}
